db3ee70e775b3d8a13be02e67c3be78edbc4c1bd
Meidimax <max.meidinger99@gmail.com>
2023-09-12 07:17:10 +0000

    add postinstall script for additional dependencies

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "Installing Dependencies..."
+
+sudo apt-get install cargo
+
+rustup target add riscv64gc-unknown-none-elf
+
+sudo cargo install cargo-binutils
+
+rustup component add llvm-tools-preview
+
+sudo apt-get install -y qemu-system 
+
+sudo apt-get install -y gdb-multiarch
